39398ab4ed249e3b4f8cd624fa73e926023adba3
vyashramjatan <vyash.ramjatan@student.hu.nl>
2025-04-08 11:52:16 +0200

    test...vyash

--- public/javascripts/User.js
+++ public/javascripts/User.js
@@ -1,12 +1,1 @@
-/*function rankuitleg() {
-    document.querySelector(".modecirkel").classList.toggle("active");
-    document.querySelector(".uitleg").classList.toggle("active");
-    
-}
-document.addEventListener("DOMContentLoaded", function () {
-    const uitlegElement = document.querySelector(".uitleg");
-   // uitlegElement.addEventListener("click", function () {
-   //     uitlegElement.classList.toggle("active");
-   document.querySelector(".modecirkel").addEventListener("click", () => console.log("Klik werkt!"));
-    });
-//});*/
+//test...hier werkt Vyash aan
